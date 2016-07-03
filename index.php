d5bd227f2fc1381a487bf1d3d8fb3a81313a9262
katrna <katrna@10.55.105.28>
2016-07-03 20:07:03 +0100

    @katrina updated index

--- index.php
+++ index.php
@@ -1,23 +1,16 @@
-
-
+<!--
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
-
+ */-->
 
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Blog Test Title Blog</title>
-    
-        <!-- Bootstrap Core CSS -->
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-
-        <!-- Custom CSS -->
-        <link href="css/blog-post.css" rel="stylesheet">
+   
  
     </head>
     
