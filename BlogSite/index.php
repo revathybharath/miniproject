08240db9da3124d878e0df2d521768e427cc13a9
katrna <katrna@10.55.105.28>
2016-07-04 23:11:25 +0100

    @katrina cleaning up files

--- BlogSite/index.php
+++ BlogSite/index.php
@@ -1,9 +1,3 @@
-<!--
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */-->
 
 <!DOCTYPE html>
 <html>
@@ -21,4 +15,8 @@
     
     <body>
     
-   
+        <h1> Articles </h1>
+        
+    
+    </body>
+</html>
