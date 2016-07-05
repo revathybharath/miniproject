ad12dd10eaa82d826d6ee0aad8c2160cf4fbd288
getintotech <revathysaisindu@gmail.com>
2016-07-05 10:53:51 +0100

    Header&Footer

--- BlogSite/Header&Footer/Header.php
+++ BlogSite/Header&Footer/Header.php
@@ -1,5 +1,5 @@
 <html>
   <body>
     <div class="header">
-      <img src="BlogLogo.jpeg" width="280" height="125" title="Blog Logo" alt="BlogLogo" />
+      <img src="Logo.jpeg" width="280" height="125" title="Blog Logo" alt="BlogLogo" />
     </div>
