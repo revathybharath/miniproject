266176ea356fedb5484cc13a5065f23ec3c89bd5
Tisha16 <Tisha@tishas-air>
2016-07-10 20:14:52 +0100

    Tisha

--- BlogSite/web/Footer.php
+++ BlogSite/web/Footer.php
@@ -2,7 +2,18 @@
     <br><br>
     <hr>
     <div class="footer">
+  
+                         <!--Subscription Form -->
+    <form action="template/subcribe_form.php" method="post">
+    <span><font face="verdana"color="white">Please enter email to receive the latest posts</font></span>
+    <input name="email" class="email" type="text">
+    <button type="submit" class="btn_email">Send</button>
+</form>
+                        <!-- End Subscription Form -->
+  
       © 2016 Osterley Guide ALL RIGHTS RESERVED
+      
+  
     </div>
   </body>
 </html>
