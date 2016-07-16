b0107ff3b6ebd0a69b7720f4b7caf26f107a70ea
katrna <katrna@10.55.105.28>
2016-07-16 18:13:40 +0100

    Added rich text editor to submit blog form

--- BlogSite/web/createBlogPost.php
+++ BlogSite/web/createBlogPost.php
@@ -1,13 +1,14 @@
  <link rel="stylesheet" href="/css/view-article.css">
-     
-     <?php 
+ <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+ <script>tinymce.init({ selector:'textarea' });</script>
+
+ <?php
 include("header.php"); 
 include 'common.php';
 include 'View/blog_view.php';
 include dirname(__FILE__).'/../dao/BlogDao.php';
 
 use function View\display;
-
 
 
 
