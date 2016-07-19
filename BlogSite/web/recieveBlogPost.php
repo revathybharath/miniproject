8eb4efc52fabb798e22098c25205b0b6f4bc3812
katrna <katrna@10.55.105.28>
2016-07-19 21:16:43 +0100

    Must be logged in to post, recognises author if logged in

--- BlogSite/web/recieveBlogPost.php
+++ BlogSite/web/recieveBlogPost.php
@@ -8,12 +8,14 @@ use function App\create_post;
 
 $articleTitle = $_POST['articleTitle'];
 $articleData = $_POST['articleData'];
+$UserId = $_POST['articleUser'];
+//$articleAuthor = $_Post['articleAuthor'];
 
 
 //echo $articleData;
 //echo $articleTitle;
 //($ArticleId, $CategoryId, $UserId, $PostTitle, $CreatedOn, $Content, $RoleID)
-$blg = new Article (1, 1, 1, $articleTitle, date('Y-m-d H:i:s'), $articleData,1);
+$blg = new Article (1, 1, $UserId, $articleTitle, date('Y-m-d H:i:s'), $articleData,1);
 
 //echo $blg -> getPostTitle();
 
