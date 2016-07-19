0104da33484949d9f7be275c5be7d690a163c26e
Tisha16 <Tisha@tishas-air>
2016-07-19 07:10:32 +0100

    about_us

--- BlogSite/web/AboutUs.php
+++ BlogSite/web/AboutUs.php
@@ -3,19 +3,72 @@ include("header.php"); ?>
 
 <html>
     <title>About Us</title>
-    
-    <body>
-        <h1>About Us</h1>
-        <p><font face="verdana">You often read stories of entrepreneurs who came up with their million pound ideas<br> over a couple of beers with friends. Not us. 
+<style>
+div.container {
+    width: 100%;
+    border: 0px solid gray;
+}
+
+p {
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+nav {
+    float: left;
+    max-width: 160px;
+    margin: 0;
+    padding: 1em;
+    font-family: Arial;
+
+}
+
+nav ul {
+    list-style-type: none;
+    padding: 0;
+}
+   
+nav ul a {
+    text-decoration: none;
+}
+
+article {
+    margin-left: 170px;
+    border-left: 5px solid pink;
+    padding: 1em;
+    overflow: hidden;
+}
+</style>
+   </head>
+<body>
+
+<div class="container">
+
+<header>
+   <h1><p>Our story so far...</p></h1>
+</header>
+<nav>
+  <ul>
+    <li>Revathy</li>
+    <li>Connie</li>
+    <li>Katrina</li>
+    <li>Tisha</li>
+
+  </ul>
+</nav>     
+     
+<article>
+      <p>You often read stories of entrepreneurs who came up with their million pound ideas<br> over a couple of beers with friends. Not us. 
       You even hear those feel good stories of <br>university roommates and best friends who started a company together. Not us either. 
       <br>
       <br>
       We came together because we were on a course that promised to get women into <br>technology and our group task was to create a blog. 
       <br>
       <br>
-      So here we are The DevelopHers and this is our blog! </font></p>
-    </body>
+      So here we are the DevelopHers and this is our blog! </p>
+</article>
+</div>
+
 </html>
 
-
+    
 <?php include("Footer.php"); ?>
