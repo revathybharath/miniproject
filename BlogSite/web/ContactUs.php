568204d0aead3c59e7a1b00fec01a5fc9e434eb2
Connie <Adrian@HOME>
2016-07-20 13:30:00 +0100

    Conatct Us form modifications inc clear button

--- BlogSite/web/ContactUs.php
+++ BlogSite/web/ContactUs.php
@@ -1,14 +1,14 @@
 <?php
 include("header.php"); 
 ?>
-<form name="contactform" method="post" action="send_form_email.php"> 
+<form name="contactform" method="post" action="mail.php"> 
 <table width="450px">
 <tr>
  <td valign="top">
   <label for="first_name">First Name *</label>
  </td>
  <td valign="top">
-  <input  type="text" name="first_name" maxlength="50" size="30">
+  <input  type="text" name="name" maxlength="50" size="30">
  </td>
 </tr>
 <tr>
@@ -40,7 +40,7 @@ include("header.php");
   <label for="comments">Comments *</label>
  </td>
  <td valign="top">
-  <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+  <textarea  name="message" maxlength="1000" cols="25" rows="6"></textarea>
  </td>
 </tr>
 <tr>
@@ -48,11 +48,13 @@ include("header.php");
      <input type="submit" value="Submit">
  </td>
 </tr>
+<tr>
+ <td colspan="2" style="text-align:center">
+     <input type="reset" value="Clear">
+ </td>
+</tr>
 </table>
 </form>
-
-
-
 
 <?php include("Footer.php"); ?>
 
